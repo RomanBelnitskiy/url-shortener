cce6cd42c948a193239c33c0bd627a959b799eec
Ivor Rudenko <rudenko23@gmail.com>
2024-01-13 23:44:37 +0200

    Створив генератор

---
+++ src/main/java/com/example/urlshortener/service/generator/Generator.java
@@ -0,0 +1,9 @@
+package com.example.urlshortener.service.generator;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Generator {
+    public static String generateShortLink() {
+        // Генеруємо випадковий рядок довжиною 8 символів
+        return RandomStringUtils.randomAlphanumeric(8);
+    }
+}
