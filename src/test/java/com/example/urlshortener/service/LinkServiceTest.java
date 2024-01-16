ba52af8e3b13a87b37ddb629b987c1745b695451
Тимченко Влад <timchenko.vld@gmail.com>
2024-01-16 22:25:26 +0200

    fix create test

--- src/test/java/com/example/urlshortener/service/LinkServiceTest.java
+++ src/test/java/com/example/urlshortener/service/LinkServiceTest.java
@@ -16,6 +16,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -25,6 +27,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class LinkServiceTest {
     @Mock
     private LinkRepository repository;
