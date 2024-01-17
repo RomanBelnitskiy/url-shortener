9f86f0fb11d3b0325102eae8248d753502f9e2cd
Roman Belnitskiy <roman.belnitskiy@gmail.com>
2024-01-17 23:23:09 +0200

    updated MainControllerTest

--- src/main/java/com/example/urlshortener/controller/MainController.java
+++ src/main/java/com/example/urlshortener/controller/MainController.java
@@ -9,8 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+
+import static org.springframework.http.HttpStatus.*;
 
 @Controller
 public class MainController {
@@ -25,13 +28,27 @@ public class MainController {
             return new RedirectView(linkDto.getLongUrl());
     }
 
-    @ExceptionHandler({
-            LinkNotFoundException.class,
-            LinkExpiredException.class,
-            IllegalArgumentException.class
-    })
-    public ModelAndView exceptionHandler(Exception ex) {
+    @ExceptionHandler(LinkNotFoundException.class)
+    @ResponseStatus(NOT_FOUND)
+    public ModelAndView notFoundExceptionHandler(Exception ex) {
+        return getModelAndView(ex, NOT_FOUND.value());
+    }
+
+    @ExceptionHandler(LinkExpiredException.class)
+    @ResponseStatus(GONE)
+    public ModelAndView expiredExceptionHandler(Exception ex) {
+        return getModelAndView(ex, GONE.value());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ModelAndView illegalArgumentExceptionHandler(Exception ex) {
+        return getModelAndView(ex, BAD_REQUEST.value());
+    }
+
+    private ModelAndView getModelAndView(Exception ex, int statusCode) {
         ModelAndView model = new ModelAndView("error");
+        model.addObject("statusCode", statusCode);
         model.addObject("message", ex.getLocalizedMessage());
 
         return model;
