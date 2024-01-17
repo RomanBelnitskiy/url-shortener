de0f9eb065051fca508a923abf08babda1db6706
Alexandr <alexandr.bosenko@gmail.com>
2024-01-17 11:55:14 +0200

    fix Generator test

--- src/test/java/com/example/urlshortener/service/generator/GeneratorTest.java
+++ src/test/java/com/example/urlshortener/service/generator/GeneratorTest.java
@@ -1,41 +1,35 @@
 package com.example.urlshortener.service.generator;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GeneratorTest {
-    private static final int LOOP_COUNT = 1000;
-    private Generator generator = new Generator();
-    @Test
-    void testGenerateShortLinkV1() {
-        final char[] testChars = {'a', 'z', 'A', 'Z', '0', '9'};
-        boolean[] found = {false, false, false, false, false, false};
-
-        for (int i = 0; i < LOOP_COUNT; i++) {
-            String randShortLink = generator.generateShortLink();
-            for (int j = 0; j < testChars.length; j++) {
-                if (randShortLink.indexOf(testChars[j]) > 0) {
-                    found[j] = true;
-                }
-            }
-        }
-
-        for (int i = 0; i < testChars.length; i++) {
-            assertTrue(found[i], String.format("alphanumeric character not generated in %d attempts", LOOP_COUNT));
-        }
+    private static Generator generator;
+    private static Pattern pattern;
+    @BeforeAll
+    static void init() {
+        generator = new Generator();
+        pattern = Pattern.compile("[A-Za-z0-9]{8}");
     }
-
-    @Test
-    void testGenerateShortLinkV2() {
-        Pattern pattern = Pattern.compile("[A-Za-z0-9]{8}");
-        String randShortLink = generator.generateShortLink();
+    @ParameterizedTest
+    @MethodSource("randomShortLinkProvider")
+    void testGenerateShortLink(String randShortLink) {
         Matcher matcher = pattern.matcher(randShortLink);
 
         assertTrue(matcher.matches());
+    }
+    static Stream<String> randomShortLinkProvider() {
+        return Stream
+                .generate(generator::generateShortLink)
+                .limit(10);
     }
 }
