38f7ef862a405349f46a797400204ec235b65953
Тимченко Влад <timchenko.vld@gmail.com>
2024-01-18 16:44:48 +0200

    added user repository

---
+++ src/main/java/com/example/urlshortener/data/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.urlshortener.data.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+}
