f4a5e780a80ef88451f2c9d98ae43751671d24e9
Тимченко Влад <timchenko.vld@gmail.com>
2024-01-19 00:55:57 +0200

    fix user repository

--- src/main/java/com/example/urlshortener/data/repository/UserRepository.java
+++ src/main/java/com/example/urlshortener/data/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.urlshortener.data.repository;
 
+import com.example.urlshortener.data.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
