41e8f83302586e50197051e2c0ba83e453adfe3b
teodorposhyvak <poshyvak.teodor.t@gmail.com>
2024-01-19 10:26:34 +0200

    LinkControllerTest fix

--- src/test/java/com/example/urlshortener/controller/LinkControllerTest.java
+++ src/test/java/com/example/urlshortener/controller/LinkControllerTest.java
@@ -6,23 +6,15 @@ import com.example.urlshortener.controller.response.LinkResponse;
 import com.example.urlshortener.mapper.LinkMapper;
 import com.example.urlshortener.service.dto.LinkDto;
 import com.example.urlshortener.service.service.LinkService;
-import com.example.urlshortener.service.service.impl.LinkServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.hamcrest.Matchers;
-
-import static org.hamcrest.Matchers.is;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -30,7 +22,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
@@ -54,7 +45,110 @@ class LinkControllerTest {
     }
 
     @Test
+    @DisplayName("Should get all link")
     void getAllLink() throws Exception {
+        List<LinkDto> linkDtos = getlinkDtos();
+        List<LinkResponse> linkResponses = getLinkResponses();
+
+        Mockito.when(linkService.findAll()).thenReturn(linkDtos);
+        Mockito.when(linkMapper.toResponses(linkDtos)).thenReturn(linkResponses);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/link"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+
+    @Test
+    @DisplayName("Should get link by valid id")
+    void getLinkByShortUrl() throws Exception {
+        String shortUrl = "example";
+        String longUrl = "http://example.com";
+
+        LinkDto linkDto = new LinkDto();
+        linkDto.setShortUrl(shortUrl);
+        linkDto.setLongUrl(longUrl);
+
+        LinkResponse linkResponse = new LinkResponse();
+        linkResponse.setShortUrl(shortUrl);
+        linkResponse.setLongUrl(longUrl);
+
+        Mockito.when(linkService.getByShortUrl(shortUrl)).thenReturn(linkDto);
+        Mockito.when(linkMapper.toResponse(linkDto)).thenReturn(linkResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/link/{shortLink}", shortUrl))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(jsonPath("$.shortUrl").value(linkResponse.getShortUrl()))
+                .andExpect(jsonPath("$.longUrl").value(linkResponse.getLongUrl()))
+                .andExpect(jsonPath("$.createdAt").value(linkResponse.getCreatedAt()))
+                .andExpect(jsonPath("$.expiredAt").value(linkResponse.getExpiredAt()))
+                .andExpect(jsonPath("$.transitions").value(linkResponse.getTransitions()));
+
+    }
+
+    @Test
+    @DisplayName("Should create link if not exists link")
+    void createLink() throws Exception {
+        CreateLinkRequest createLinkRequest = new CreateLinkRequest();
+        createLinkRequest.setLongUrl("http://example.com");
+        createLinkRequest.setExpiredAt(LocalDateTime.now());
+
+        LinkResponse linkResponse = new LinkResponse();
+        linkResponse.setLongUrl("http://example.com");
+        linkResponse.setExpiredAt(LocalDateTime.now());
+
+        LinkDto createdLinkDto = linkMapper.toDto(createLinkRequest);
+        Mockito.when(linkService.create(Mockito.any())).thenReturn(createdLinkDto);
+        Mockito.when(linkMapper.toResponse(createdLinkDto)).thenReturn(linkResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/link")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createLinkRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(jsonPath("$.shortUrl").value(linkResponse.getShortUrl()))
+                .andExpect(jsonPath("$.longUrl").value(linkResponse.getLongUrl()));
+    }
+
+    @Test
+    @DisplayName("Should update link by valid id")
+    void updateLink() throws Exception {
+        String shortLink = "example";
+        UpdateLinkRequest linkRequest = new UpdateLinkRequest();
+        linkRequest.setLongUrl("http://exampleUpdate.com");
+        linkRequest.setExpiredAt(LocalDateTime.now().plusMinutes(1));
+
+        LinkDto linkDto = linkMapper.toDto(shortLink, linkRequest);
+        Mockito.doNothing().when(linkService).update(linkDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/link/{shortLink}", shortLink)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(linkRequest))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+
+    @Test
+    @DisplayName("Should delete link by valid id")
+    void deleteLink() throws Exception {
+        String shortUrl = "example";
+        String longUrl = "http://example.com";
+
+        LinkDto linkDto = new LinkDto();
+        linkDto.setShortUrl(shortUrl);
+        linkDto.setLongUrl(longUrl);
+
+        Mockito.doNothing().when(linkService).deleteByShortUrl(shortUrl);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/link/{shortLink}", shortUrl))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+
+
+    public List<LinkDto> getlinkDtos() {
         LinkDto linkDto = new LinkDto("example",
                 "http://exemple.com",
                 LocalDateTime.now(),
@@ -67,79 +161,22 @@ class LinkControllerTest {
                 LocalDateTime.now().plusMinutes(4),
                 1);
 
-        List<LinkDto> linkEntities = List.of(linkDto, linkDtoSecond);
-        List<LinkResponse> linkResponses = linkMapper.toResponses(linkEntities);
-
-        Mockito.when(linkService.findAll()).thenReturn(linkEntities);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/link"))
-                .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(jsonPath("$.size()", Matchers.is(linkResponses.size())));
+        return List.of(linkDto, linkDtoSecond);
     }
 
-    @Test
-    void getLinkByShortUrl() throws Exception {
-        String shortUrl = "example";
-        String longUrl = "http://example.com";
+    public List<LinkResponse> getLinkResponses() {
+        LinkResponse linkResponse = new LinkResponse("example",
+                "http://exemple.com",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(4),
+                0L);
 
-        LinkDto linkDto = new LinkDto();
-        linkDto.setShortUrl(shortUrl);
-        linkDto.setLongUrl(longUrl);
-
-        Mockito.when(linkService.getByShortUrl(shortUrl)).thenReturn(linkDto);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/link/{shortLink}", shortUrl))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-
-    }
-
-    @Test
-    void createLink() throws Exception {
-        CreateLinkRequest createLinkRequest = new CreateLinkRequest();
-        createLinkRequest.setLongUrl("http://example.com");
-        createLinkRequest.setExpiredAt(LocalDateTime.now().plusMinutes(1));
-
-        LinkDto createdLinkDto = linkMapper.toDto(createLinkRequest);
-        Mockito.when(linkService.create(Mockito.any())).thenReturn(createdLinkDto);
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/link")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(createLinkRequest))
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-    }
-
-    @Test
-    void updateLink() throws Exception {
-        String shortLink = "example";
-        UpdateLinkRequest linkRequest = new UpdateLinkRequest();
-        linkRequest.setLongUrl("http://example.com");
-        linkRequest.setExpiredAt(LocalDateTime.now().plusMinutes(1));
-
-        LinkDto linkDto = linkMapper.toDto(shortLink, linkRequest);
-        Mockito.doNothing().when(linkService).update(linkDto);
-
-        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/link/{shortLink}", shortLink)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(linkRequest))
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
-    }
-
-    @Test
-    void deleteLink() throws Exception {
-        String shortUrl = "example";
-        String longUrl = "http://example.com";
-
-        LinkDto linkDto = new LinkDto();
-        linkDto.setShortUrl(shortUrl);
-        linkDto.setLongUrl(longUrl);
-
-        Mockito.doNothing().when(linkService).deleteByShortUrl(shortUrl);
-
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/link/{shortLink}", shortUrl))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+        LinkResponse linkResponse1 = new LinkResponse("example2",
+                "http://exempleSecond.com",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(4),
+                1L);
+        return List.of(linkResponse, linkResponse1);
     }
 }
+
