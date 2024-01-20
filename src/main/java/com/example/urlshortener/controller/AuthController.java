edfd4f7b63c83dfea286dabcd798fb33c078634f
Тимченко Влад <timchenko.vld@gmail.com>
2024-01-21 00:30:15 +0200

    added authenticate controller

---
+++ src/main/java/com/example/urlshortener/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.urlshortener.controller;
+
+import com.example.urlshortener.controller.request.RegisterRequest;
+import com.example.urlshortener.controller.request.auth.AuthRequest;
+import com.example.urlshortener.controller.response.auth.AuthResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
