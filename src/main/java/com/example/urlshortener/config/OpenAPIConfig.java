ab7dcd624dfab8f060331dc94a8c7ab4b195feb9
Roman Belnitskiy <roman.belnitskiy@gmail.com>
2024-01-21 23:11:33 +0200

    fix openAPI config

--- src/main/java/com/example/urlshortener/config/OpenAPIConfig.java
+++ src/main/java/com/example/urlshortener/config/OpenAPIConfig.java
@@ -1,10 +1,16 @@
 package com.example.urlshortener.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
@@ -14,7 +20,11 @@ public class OpenAPIConfig {
             @Value("${app.description}") String appDescription,
             @Value("${app.version}") String appVersion) {
 
-        return new OpenAPI()
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+        securityRequirement.addList("bearerAuth", Collections.emptyList());
+        Components components = new Components();
+        components.addSecuritySchemes("bearerAuth", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"));
+        return new OpenAPI().components(components).security(List.of(securityRequirement))
                 .info(new Info()
                         .title(appTitle)
                         .version(appVersion)
