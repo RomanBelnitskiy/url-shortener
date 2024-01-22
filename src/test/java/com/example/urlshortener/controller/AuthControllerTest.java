6051a4de26abc5fed6520edc76aaafc3a7721632
Тимченко Влад <timchenko.vld@gmail.com>
2024-01-22 15:41:44 +0200

    added authenticate controller tests

---
+++ src/test/java/com/example/urlshortener/controller/AuthControllerTest.java
@@ -0,0 +1,68 @@
+package com.example.urlshortener.controller;
+
+import com.example.urlshortener.config.AuthFilter;
+import com.example.urlshortener.controller.request.RegisterRequest;
+import com.example.urlshortener.controller.request.auth.AuthRequest;
+import com.example.urlshortener.controller.response.auth.AuthResponse;
+import com.example.urlshortener.service.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(
+        value = AuthController.class,
+        excludeAutoConfiguration = SecurityAutoConfiguration.class,
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = AuthFilter.class)}
+)
+class AuthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private AuthService authService;
+
+    @Test
+    @DisplayName("Should register user and return token")
+    public void registerUser_AndReturnToken() throws Exception {
+        RegisterRequest request = new RegisterRequest("aaa123", "testPass");
+        AuthResponse authResponse = new AuthResponse("mocked-jwt-token");
+
+        when(authService.register(any(RegisterRequest.class))).thenReturn(authResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.token").value("mocked-jwt-token"));
+    }
+
+    @Test
+    @DisplayName("Should authenticate user")
+    void authenticateUser() throws Exception {
+        RegisterRequest request = new RegisterRequest("aaa123", "testPass");
+        AuthResponse authResponse = new AuthResponse("mocked-jwt-token");
+
+        when(authService.authenticate(any(AuthRequest.class))).thenReturn(authResponse);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/auth/authenticate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.token").value("mocked-jwt-token"));
+    }
+}
